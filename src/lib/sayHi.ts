b72bea6098845c098045e4e8143b34b30543e354
GustaveWPM <jgeorge@perso.be>
2025-04-26 18:36:29 +0200

    chore: typo

--- src/lib/sayHi.ts
+++ src/lib/sayHi.ts
@@ -2,7 +2,7 @@ import type { HELLO_MODES, HelloMode } from '@me/shared-types/Hello';
 
 import { sayHello } from '@me/shared-lib/sayHello';
 
-// * ... Since people who says "hi" are the most polite, the compiler would be surprise if we ask to yell here
+// * ... Since people who says "hi" are the most polite, the compiler would be surprised if we ask to yell here
 type SayHiAvailableModes = Exclude<HelloMode, typeof HELLO_MODES.yell>;
 
 export const sayHi = (params: { mode: SayHiAvailableModes }) => sayHello(params);
