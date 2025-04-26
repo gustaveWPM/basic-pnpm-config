c0cd9dae6f92b9b77ef2a9c800333f036fdc1313
GustaveWPM <jgeorge@perso.be>
2025-04-26 18:29:25 +0200

    refactor: magic strings instead of symbols

--- packages/shared-lib/src/sayHello.ts
+++ packages/shared-lib/src/sayHello.ts
@@ -1,13 +1,13 @@
 import type { HelloMode } from '@me/shared-types/Hello';
 
-import { helloModes } from '@me/shared-types/Hello';
+import { HELLO_MODES } from '@me/shared-types/Hello';
 
 const HELLO_MSG = 'Hello';
 
 const MATCHING_TRANSFORMATIONS = {
-  [helloModes.yellMode]: (string: string) => string.toUpperCase(),
-  [helloModes.shyMode]: (string: string) => string.toLowerCase(),
-  [helloModes.neutralMode]: (string: string) => string
+  [HELLO_MODES.yellMode]: (string: string) => string.toUpperCase(),
+  [HELLO_MODES.shyMode]: (string: string) => string.toLowerCase(),
+  [HELLO_MODES.neutralMode]: (string: string) => string
 } as const satisfies Record<HelloMode, (string: string) => string>;
 
 export const sayHello = ({ mode }: { mode: HelloMode }) => console.log(MATCHING_TRANSFORMATIONS[mode](HELLO_MSG));
--- packages/shared-types/src/Hello.ts
+++ packages/shared-types/src/Hello.ts
@@ -1,11 +1,7 @@
-const neutralMode = Symbol();
-const yellMode = Symbol();
-const shyMode = Symbol();
-
-export const helloModes = {
-  neutralMode,
-  yellMode,
-  shyMode
+export const HELLO_MODES = {
+  neutralMode: 'N',
+  yellMode: 'Y',
+  shyMode: 'S'
 } as const;
 
-export type HelloMode = (typeof helloModes)[keyof typeof helloModes];
+export type HelloMode = (typeof HELLO_MODES)[keyof typeof HELLO_MODES];
